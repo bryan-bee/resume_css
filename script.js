12b38ad586b97cec04db358e8883cb8a9d7ba12e
bryan-bee <bryanbee1717@gmail.com>
2023-09-30 18:56:49 -0700

    final changes

--- script.js
+++ script.js
@@ -27,17 +27,28 @@ const imageContainer = document.querySelector('.image-container');
 const audio = document.getElementById('audio');
 const imageElement = document.getElementById('image');
 const dialogueContainer = document.querySelector('.dialogue-container'); // Add this line
+const resume = document.querySelector('.resume')
 let isImage1 = true;
 let isImageClicked = false;
+// Variable to track if the image is wiggling
+let isImageWiggling = false;
+let isColorChanging = false;
+let isSpinning = false;
 
 imageContainer.addEventListener('click', () => {
     // Hide the "Click me" text
     const clickMeText = document.querySelector('.click-me');
     clickMeText.style.display = 'none';
-
-    // Start automatic color change
-    startColorChangeInterval();
-
+    if (!isColorChanging){
+        // Start automatic color change
+        startColorChangeInterval();
+        isColorChanging = true;
+    }else{
+        stopColorChangeInterval();
+        document.querySelector('.main_container').style.backgroundColor = 'white'
+        isColorChanging = false;
+    }
+    
     if (!isAudioPlaying) {
         // Play the audio
         audio.loop = true;
@@ -45,14 +56,19 @@ imageContainer.addEventListener('click', () => {
             console.error('Audio playback error:', error);
         });
         isAudioPlaying = true;
+    }else{
+        audio.pause();
+        isAudioPlaying = false;
     }
-
+    
     if (isImage1) {
         imageElement.src = 'wake.jpg'; // Change to the second image
         imageElement.alt = 'Image 2'; // Update alt text if needed
+        resume.classList.add('wiggle');
     } else {
-        imageElement.src = 'original.jpg'; // Change back to the first image
+        imageElement.src = '379907128_792514029339391_3760204760494117783_n (1).jpg'; // Change back to the first image
         imageElement.alt = 'Image 1'; // Update alt text if needed
+        resume.classList.remove('wiggle');
     }
     
     isImage1 = !isImage1; // Toggle the image flag
@@ -66,4 +82,7 @@ imageContainer.addEventListener('click', () => {
         dialogueContainer.style.top = '100px'; // Adjust the top position as needed
         dialogueContainer.style.left = '300px'; // Adjust the left position as needed
     }
+    
+
 });
+
